1ffadbbe69a7134f354d5bf8c51ffd427baba9e5
masachetti <50852444+masachetti@users.noreply.github.com>
2022-06-30 14:09:22 -0400

    Add the main file/method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,43 @@
-import bank.model.Client;
+import bank.Bank;
+import bank.model.account.Account;
+import bank.model.account.CheckingAccount;
+import bank.model.account.SavingAccount;
+import bank.model.client.Client;
+import bank.model.client.Corporation;
+import bank.model.client.Individual;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        Individual ip1 = new Individual("Joao", "123.245.111-30");
+        Individual ip2 = new Individual("Maria", "123.222.666-00");
+        Corporation le1 = new Corporation("Satt", "65.889.594/0001-89");
+        Corporation le2 = new Corporation("Mercadinho Sao Joao", "75.027.815/0001-63");
+        CheckingAccount ca = new CheckingAccount(1001, 101, le1);
+        SavingAccount sa = new SavingAccount(1001, 102, le1);
+
+//        le1.setAccounts(new HashSet<Account>(){{
+//            add(ca);
+//            add(sa);
+//        }});
+
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Bank-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Bank bank = new Bank(entityManager);
+        bank.addNewClient(ip1);
+        bank.addNewClient(ip2);
+        bank.addNewClient(le1);
+        bank.addNewClient(le2);
+        bank.assignAccountToClient(ip1, ca);
+        bank.assignAccountToClient(ip1, sa);
 
-
-        entityManager.getTransaction().begin();
-        Client cliente = new Client("Carlos");
-        entityManager.persist(cliente);
-        entityManager.getTransaction().commit();
-
+        bank.showAllClients();
+        bank.showAllAccounts();
 
         entityManager.close();
         entityManagerFactory.close();
